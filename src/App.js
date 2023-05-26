d4b745ec406a588367fe02a7c04d64db272e8c3d
WooJi-Shin <121946458+WooJi-Shin@users.noreply.github.com>
2023-05-26 21:59:12 +0900

    ToDO 연습

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import Button from "./Button";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  const deleteBtn = (event) => {
+    setToDos(toDos.filter((_, toDoIndex) => event !== toDoIndex));
+  };
+  console.log(toDos);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-      <Button text={"Continue"} />
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} text="text" placeholder="Write your to do..."></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteBtn(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
