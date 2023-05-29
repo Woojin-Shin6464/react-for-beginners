fad9f6fe969e941634ddcfaf014db6d5a3b52bcc
WooJi-Shin <121946458+WooJi-Shin@users.noreply.github.com>
2023-05-29 12:03:26 +0900

    react-router-dom 연습

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-import { useState } from "react";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  const deleteBtn = (event) => {
-    setToDos(toDos.filter((_, toDoIndex) => event !== toDoIndex));
-  };
-  console.log(toDos);
   return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} text="text" placeholder="Write your to do..."></input>
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => deleteBtn(index)}>❌</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
   );
 }
 
