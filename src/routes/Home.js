479e6d04810779eace118321df1a1f27944804cc
WooJi-Shin <121946458+WooJi-Shin@users.noreply.github.com>
2023-05-29 18:15:38 +0900

    react-router-dom Publishing

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,7 +5,7 @@ function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)).json();
+    const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=10&sort_by=year`)).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
@@ -19,7 +19,7 @@ function Home() {
       ) : (
         <div>
           {movies.map((movie) => (
-            <Movie key={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
+            <Movie key={movie.id} id={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
           ))}
         </div>
       )}
