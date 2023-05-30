0a42d533ba67a68eb295f46e5097facb9415cb2a
WooJi-Shin <121946458+WooJi-Shin@users.noreply.github.com>
2023-05-30 14:32:17 +0900

    basename 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
-      <img src={coverImg} alt={title} />
+      <Link to={`/movie/${id}`}>
+        <img src={coverImg} alt={title} />
+      </Link>
       <h2>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -5,7 +5,6 @@ function Detail() {
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
-    console.log(json);
   };
   useEffect(() => {
     getMovie();
